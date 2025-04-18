dc8a6d0ef038b5ba6aeb221838cdd90e7246b2f8
notzree <richard@symphoniclabs.com>
2025-04-18 02:01:03 -0700

    fix version

--- Formula/rprompt.rb
+++ Formula/rprompt.rb
@@ -4,10 +4,10 @@ class Rprompt < Formula
   version "0.1.0" # Update this with your actual version
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/notzree/rprompt/releases/download/v0.1.0/rprompt-darwin-arm64.tar.gz"
+    url "https://github.com/notzree/rprompt/releases/download/v0.1.1/rprompt-darwin-arm64.tar.gz"
     sha256 "058f00da84f4624f4656a920df0d1f48a3cfec8c181c86b5ea2805afbab4acb1"
   elsif OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/notzree/rprompt/releases/download/v0.1.0/rprompt-darwin-amd64.tar.gz"
+    url "https://github.com/notzree/rprompt/releases/download/v0.1.1/rprompt-darwin-amd64.tar.gz"
     sha256 "ab4dfcb1df0d3c6fb19ffc22dd8f0960d8faa7714f378c1769b122bfdd4dc10e"
   end
 
